d6bd7c61839b8da85556d51a9ba923b3370fdb14
JP Caparas <jp@jpcaparas.com>
2024-02-21 21:57:49 +1300

    Force SSL when using a tunnel and built in server

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Http\Request;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
+use OneLogin\Saml2\Utils as Saml2Utils;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,8 +20,20 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot(Request $request): void
     {
-        //
+        if ($this->isUsingBuiltInServer() && $request->headers->has('x-forwarded-host')) {
+            $protocol = $request->headers->get('x-forwarded-proto');
+            if ($protocol === 'https') {
+                Saml2Utils::setSelfProtocol('https');
+                URL::forceScheme($protocol);
+            }
+        }
+    }
+
+    private function isUsingBuiltInServer(): bool
+    {
+        return str_contains($_SERVER['SERVER_SOFTWARE'], 'PHP') &&
+            str_contains($_SERVER['SERVER_SOFTWARE'], 'Development Server');
     }
 }
